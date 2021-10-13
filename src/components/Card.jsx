f5efdd455268f9e2bb5a1ad46c66ee69c6407e00
Raphael Taglialegna <raphael.trcosta@gmail.com>
2021-10-13 15:25:34 -0300

    Requisit 3

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,51 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 // import Form from './Form';
 
 class Card extends React.Component {
   render() {
+    const { cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+    } = this.props;
     return (
       <div className="is-flex is-align-content-center">
-        <div className="card is-flex">Card</div>
+        <div className="card is-flex">
+          <h2 data-testid="name-card">{cardName}</h2>
+          <img data-testid="image-card" src={ cardImage } alt={ cardName } />
+          <p data-testid="description-card">{cardDescription}</p>
+          <ul>
+            <li data-testid="attr1-card">{cardAttr1}</li>
+            <li data-testid="attr2-card">{cardAttr2}</li>
+            <li data-testid="attr3-card">{cardAttr3}</li>
+          </ul>
+          <p data-testid="rare-card">{cardRare}</p>
+          <div>
+            {
+              cardTrunfo === true ? <p data-testid="trunfo-card">Super Trunfo</p> : <p />
+            }
+          </div>
+
+        </div>
       </div>
     );
   }
 }
+Card.propTypes = {
+  cardName: PropTypes.string.isRequired,
+  cardDescription: PropTypes.string.isRequired,
+  cardAttr1: PropTypes.string.isRequired,
+  cardAttr2: PropTypes.string.isRequired,
+  cardAttr3: PropTypes.string.isRequired,
+  cardImage: PropTypes.string.isRequired,
+  cardRare: PropTypes.string.isRequired,
+  cardTrunfo: PropTypes.bool.isRequired,
+};
 
 export default Card;
